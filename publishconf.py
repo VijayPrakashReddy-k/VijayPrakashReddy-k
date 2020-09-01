9ba6e3e1cf2b1d47788c40b7ca414d1448597c46
VijayPrakash <42317258+VijayPrakashReddy-k@users.noreply.github.com>
2020-09-01 14:21:13 +0530

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -12,7 +12,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://www.vijayprakashk.com/'
 RELATIVE_URLS = False
 
 GOOGLE_ANALYTICS = "UA-176931813-1"
